4c96bc1d44a3b933461db27abb18882c1397a5cf
SunskyXH <y445360776@qq.com>
2016-05-10 01:53:23 +0800

    add new resources

--- galgame_spider/spiders/mygalgame_spider.py
+++ galgame_spider/spiders/mygalgame_spider.py
@@ -18,6 +18,16 @@ class MyGalgameSpider(scrapy.Spider):
         "http://www.mygalgame.com/page/8/",
         "http://www.mygalgame.com/page/9/",
         "http://www.mygalgame.com/page/10/",
+        "http://www.mygalgame.com/page/11/",
+        "http://www.mygalgame.com/page/12/",
+        "http://www.mygalgame.com/page/13/",
+        "http://www.mygalgame.com/page/14/",
+        "http://www.mygalgame.com/page/15/",
+        "http://www.mygalgame.com/page/16/",
+        "http://www.mygalgame.com/page/17/",
+        "http://www.mygalgame.com/page/18/",
+        "http://www.mygalgame.com/page/19/",
+        "http://www.mygalgame.com/page/20/",
     ]
 
     def parse(self, response):
@@ -28,12 +38,10 @@ class MyGalgameSpider(scrapy.Spider):
             yield scrapy.Request(url, callback = self.parse_dir_contents)
 
     def parse_dir_contents(self,response):
-        for sel in response.xpath('//div[@class = "title-article"]/h4/a/text()'):
-            for sel2 in response.xpath('//div[@class = "panel panel-primary"]'):
-                item = GalgameSpiderItem()
-                item['pwd'] = sel2.xpath('//div[@class = "panel-footer"]/b//span/text()')[0:2].extract()
-                item['addr'] = sel2.xpath('//div[@class = "panel-body"]/a/button/@onclick').re('\\/pan\\.baidu\\.com\\/s\\/.*[^.)]')
-            item['title'] = sel.extract()
+        item = GalgameSpiderItem()
+        item['pwd'] = response.xpath('//div[@class = "panel panel-primary"]/div[@class = "panel-footer"]/b//span/text()')[0:2].extract()
+        item['addr'] = response.xpath('//div[@class = "panel panel-primary"]/div[@class = "panel-body"]/a/button/@onclick').re('\\/pan\\.baidu\\.com\\/s\\/.*[^.)]')
+        item['title'] = response.xpath('//div[@class = "title-article"]/h4/a/text()').extract()
         yield item
 
 
